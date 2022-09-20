a0baac96e238175fb9f688af53baa39e032ec27e
riyadmd <md.riadcse@yahoo.com>
2022-09-20 17:23:19 +0600

    Added project file

---
+++ DepartmentManagement.Database/DbContexts/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using DepartmentManagement.Models.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace DepartmentManagement.Database.DbContexts 
+{
+    
+    public class AppDbContext : DbContext
+    {
+         public AppDbContext(DbContextOptions option):base(option)
+         {
+
+         }
+        public DbSet<Person> Person { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Department> Departments { get; set; }
+
+        /*protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = @"Server=(local)\SQLEXPRESS; Database=DepartmentManagementDb; Integrated Security=true";
+            optionsBuilder.UseSqlServer(connectionString);
+        }*/
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Seed();
+        }
+
+        
+    }
+}
